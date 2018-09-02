6be585b1bb367c22955bcbd46d3489e971c83d81
chenjian <chenjian_336@126.com>
2018-09-02 13:17:18 +0800

    merge Test.java

--- src/com/git/test/Test.java
+++ src/com/git/test/Test.java
@@ -6,10 +6,13 @@ import java.util.List;
 public class Test {
 
 	public static void main(String[] args) {
+		
 		List<Person> personList = new ArrayList<>();
 		personList.add(new Person(1, "A"));
 		personList.add(new Person(2, "B"));
 		personList.add(new Person(3, "C"));
 		System.out.println(personList);
+		
+		System.out.println("Test---");
 	}
 }
