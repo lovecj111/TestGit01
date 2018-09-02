4d4771a0bcdedfd8f1a36ef38487d14935878cc3
chenjian <chenjian_336@126.com>
2018-09-02 13:15:18 +0800

    git commit 01

---
+++ src/com/git/test/Test.java
@@ -0,0 +1,15 @@
+package com.git.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+
+	public static void main(String[] args) {
+		List<Person> personList = new ArrayList<>();
+		personList.add(new Person(1, "A"));
+		personList.add(new Person(2, "B"));
+		personList.add(new Person(3, "C"));
+		System.out.println(personList);
+	}
+}
